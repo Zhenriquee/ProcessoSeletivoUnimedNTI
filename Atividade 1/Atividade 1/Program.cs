01eebd28afa3fe83c35322a88b78d73868b86246
Zhenriquee <83546851+Zhenriquee@users.noreply.github.com>
2023-11-26 11:55:15 -0300

    Add files via upload

---
+++ Atividade 1/Atividade 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Atividade1
+{
+    class Atividade1
+    {
+        static void Main(string[] args)
+{
+            quantinumero(15);
+    }
+
+static void quantinumero(int n)
+{
+    int pmNum = 0,segNum = 1;
+ Console.WriteLine("Os primeiros 15 números da sequência de Fibonacci:");
+
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write(pmNum + ", ");
+
+        int resultf = pmNum + segNum;
+            pmNum = segNum;
+            segNum = resultf;
+    }
+}
+}
+}
